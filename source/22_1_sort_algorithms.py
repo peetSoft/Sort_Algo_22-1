a630a0f08374d2519dbd8efc42a8356184e58e04
peet <teilzeitjobbewerber@gmx.de>
2022-01-19 09:34:02 +0100

    Algo improvements

--- source/22_1_sort_algorithms.py
+++ source/22_1_sort_algorithms.py
@@ -4,18 +4,33 @@ def bubble_sort(my_list):
     :param my_list:  list to be sorted and sorted list
     :return: None
     """
+    first_unsorted = 0
+    last_unsorted = len(my_list)-1
     compare_counter = 0
-    counter = 1
-    for d in range(len(my_list) - counter):
-        for i in range(len(my_list) - 1):
+    while first_unsorted < last_unsorted:
+        start_unsorted = last_unsorted
+        finish_unsorted = first_unsorted
+        step = -1
+#        start_unsorted = first_unsorted # Für den Fall von links nach rechts
+#        finish_unsorted = last_unsorted
+#        step = 1
+        new_finish_unsorted = start_unsorted
+        for i in range(start_unsorted,finish_unsorted,step):
             compare_counter += 1
-            if my_list[i] > my_list[i + 1]:
-                my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]  # Plätzetausch Pythonic way
-        counter += 1
+#            if my_list[i] > my_list[i + step]: # Für den Fall von links nach rechts
+            if my_list[i] < my_list[i + step]:
+                my_list[i], my_list[i + step] = my_list[i + step], my_list[i]  # Plätzetausch Pythonic way
+                new_finish_unsorted = i
+#        last_unsorted = new_finish_unsorted # Für den Fall von links nach rechts
+        first_unsorted = new_finish_unsorted
+
     return compare_counter
 
-#
+# 18.1.2022 -- Bubble sort mit wechsel von iterations Richtung.  Q: was bring mir das?
+# Dabei soll sich rechts und links ein sortierter Teil bilden. Die MItte bleibt unsortiert.
 
+# First = 0 , last = len()-1
+# Wie kann man sich nach einem Durchlauf merken welcher Teil der Soldaten sortiert ist?
 
 
 ## 2 Noch einen Algo: insertion algorithmus. --> Ich suche den größten und schiebe ihn nach ganz rechts.
@@ -47,9 +62,14 @@ def sort_test(sort_function):
     sorted_data7 = data7.copy()
     e = data7.pop()
     data7.insert(0, e)
-    data = [data1, data2, data3, data4, data5, data6, data7]
+    data8 = [i for i in range(101)]
+    sorted_data8 = data8.copy()
+    d = data8.pop(0)
+    data8.append(d)
+    data = [data1, data2, data3, data4, data5, data6, data7, data8]
     sorted_data = [sorted_data1, sorted_data2, sorted_data3,
-                   sorted_data4, sorted_data5, sorted_data6, sorted_data7]
+                   sorted_data4, sorted_data5, sorted_data6, sorted_data7,
+                   sorted_data8]
     print("Testing of ", sort_function.__name__)
 
     wrong_counter = 0
